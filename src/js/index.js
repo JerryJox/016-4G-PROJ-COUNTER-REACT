73f0792851b511bb9b441df58019379a9e0777ef
JerryJox <profesionalmail@msn.com>
2023-04-17 13:41:05 +0000

    update files

--- src/js/index.js
+++ src/js/index.js
@@ -4,26 +4,43 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import "bootstrap";
+import PropTypes from 'prop-types';
+
 
 //import your own components
-function SimpleCounter(props){
+
+function SecondsCounter(props){
     return (<div className="bigCounter">
-        <div className="calendar"></div>
-        <div className="six"></div>
-        <div className="five"></div>
-        <div className="four"></div>
-        <div className="three"></div>
-        <div className="two"></div>
-        <div className="one"></div>
+        <div><i className="far fa-clock"></i></div>
+        <div className="six">{props.digitSix % 10}</div>
+        <div className="five">{props.digitFive % 10}</div>
+        <div className="four">{props.digitFour % 10}</div>
+        <div className="three">{props.digitThree % 10}</div>
+        <div className="two">{props.digitTwo % 10}</div>
+        <div className="one">{props.digitOne % 10}</div>
 
     </div>);
 }
-
+SecondsCounter.prototype = {
+    digitSix: PropTypes.number,
+    digitFive: PropTypes.number,
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number,
+}
 let i = 0;
 
 setInterval(function () {
-    i++
-    console.log(i)
+    const six = Math.floor(i/100000)
+    const five = Math.floor(i/10000)
+    const four = Math.floor(i/1000)
+    const three = Math.floor(i/100)
+    const two = Math.floor(i/10)
+    const one = Math.floor(i/1)
+    console.log(six, five, four, three, two, one);
     //render your react application
-    ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+    i++;
+    ReactDOM.render(<SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six}/>, document.querySelector("#app"));
 },1000)
